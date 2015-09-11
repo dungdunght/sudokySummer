ae8d221d2173e9e6caf3f0967a32869f9e19c514
Динь Зунг Нгуен <NguyenDinhDung@MacBook-Pro-Din.local>
2015-09-11 17:11:35 +0300

    ok

---
+++ sodoku/sodoku/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  sodoku
+//
+//  Created by Динь Зунг Нгуен on 11.09.15.
+//  Copyright (c) 2015 Динь Зунг Нгуен. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
+println("this is a message")
